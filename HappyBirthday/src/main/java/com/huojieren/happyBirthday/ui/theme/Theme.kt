d325e9e8b752d6fe36ff33a55d154a8318c5baf9
huojieren <1956934061@qq.com>
2025-07-08 16:36:14 +0800

    feat(ui): 重构主题并添加生日祝福界面
    - 重命名 MyApplicationTheme 为 HappyBirthdayTheme
    - 新增 GreetingText 和 GreetingImage 组件
    - 更新 MainActivity 中的界面布局
    - 在 strings.xml 中添加生日祝福文本和签名

--- HappyBirthday/src/main/java/com/huojieren/happyBirthday/ui/theme/Theme.kt
+++ HappyBirthday/src/main/java/com/huojieren/happyBirthday/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MyApplicationTheme(
+fun HappyBirthdayTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
