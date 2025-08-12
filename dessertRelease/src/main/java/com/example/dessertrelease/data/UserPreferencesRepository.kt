225d8053799d455b11d80bfe19431f9508a51199
huojieren <1956934061@qq.com>
2025-08-12 21:30:44 +0800

    feat(persistence): 使用 DataStore存储用户布局偏好
    - 新增 UserPreferencesRepository 类，用于管理用户偏好
    - 在 DessertReleaseViewModel 中集成 UserPreferencesRepository
    - 创建 DessertReleaseApplication 类，初始化用户偏好仓库
    - 更新 AndroidManifest.xml，指定自定义 Application 类
    - 在 build.gradle.kts 中添加 DataStore 依赖

---
+++ dessertRelease/src/main/java/com/example/dessertrelease/data/UserPreferencesRepository.kt
@@ -0,0 +1,39 @@
+package com.example.dessertrelease.data
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class UserPreferencesRepository(
+    private val dataStore: DataStore<Preferences>
+) {
+    val isLinearLayout: Flow<Boolean> = dataStore.data
+        .catch {
+            if (it is IOException) {
+                Log.e(TAG, "Error reading preferences.", it)
+                emit(emptyPreferences()) // 记录错误并发出 emptyPreferences()，映射函数仍然可以映射到默认值
+            }
+        }
+        .map { preferences ->
+            // 使用映射函数将 Flow<Preferences> 转换为 Flow<Boolean>
+            preferences[IS_LINEAR_LAYOUT] ?: true
+        }
+
+    companion object {
+        val IS_LINEAR_LAYOUT = booleanPreferencesKey("is_linear_layout")
+        const val TAG = "UserPreferencesRepo"
+    }
+
+    suspend fun saveLayoutPreference(isLinearLayout: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT] = isLinearLayout
+        }
+    }
+}
