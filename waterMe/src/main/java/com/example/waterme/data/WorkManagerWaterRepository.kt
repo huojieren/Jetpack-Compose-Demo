688fdd639ed5c9ef2e1d24f245a439936ab11de6
huojieren <1956934061@qq.com>
2025-08-16 15:10:01 +0800

    feat(waterme): 添加植物浇水提醒功能
    - 实现了 WaterRepository 接口中的 scheduleReminder 方法
    - 使用 WorkManager 预定一次性提醒任务
    - 通过 Data 对象传递植物名称参数- 采用 enqueueUniqueWork 方法确保提醒任务的唯一性

--- waterMe/src/main/java/com/example/waterme/data/WorkManagerWaterRepository.kt
+++ waterMe/src/main/java/com/example/waterme/data/WorkManagerWaterRepository.kt
@@ -17,8 +17,12 @@
 package com.example.waterme.data
 
 import android.content.Context
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.waterme.model.Plant
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
 class WorkManagerWaterRepository(context: Context) : WaterRepository {
@@ -27,5 +31,19 @@ class WorkManagerWaterRepository(context: Context) : WaterRepository {
     override val plants: List<Plant>
         get() = DataSource.plants
 
-    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {}
+    override fun scheduleReminder(duration: Long, unit: TimeUnit, plantName: String) {
+        val data = Data.Builder()
+        data.putString(WaterReminderWorker.nameKey, plantName)
+
+        val workRequestBuilder = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(data.build())
+            .build()
+
+        workManager.enqueueUniqueWork(
+            plantName + duration,
+            ExistingWorkPolicy.REPLACE,
+            workRequestBuilder
+        )
+    }
 }
